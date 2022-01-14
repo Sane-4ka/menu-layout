4d03e511aec0e67f247fbef50ef4392c99bc8ea6
Sanya <Sasha12121299@gmail.com>
2022-01-14 13:50:16 +0300

    first

--- js/script.js
+++ js/script.js
@@ -1,21 +1,7 @@
-const list = document.querySelectorAll('.list');
-const indicator = document.querySelector('.indicator');
+const menu = document.querySelectorAll('.box__menu');
 
-function activeLink() {
-    list.forEach((item) => item.classList.remove('active'));
-    this.classList.add('active');
-    move(this);
-}
-
-
-list.forEach((item) => {
-    item.addEventListener('click', activeLink);
+menu.forEach((box) => {
+    box.addEventListener('click', () => {
+        box.classList.toggle('active');    
+    });
 });
-
-function move(e) {
-    for (let i = 0; i < 5; i++) {
-        if (e == list[i])  {
-            indicator.style.cssText = `transform: translateX(${70 * i}px);`;
-        }
-    }
-}
